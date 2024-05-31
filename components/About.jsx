3b895e578c4818531a8e49123da455e97cd505d9
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2024-05-30 22:48:16 -0700

    12%

--- components/About.jsx
+++ components/About.jsx
@@ -141,9 +141,17 @@ const About = () => {
     };
     
     return (
-    <section>
+    <section className='xl:h-[860px] pb-12 xl:py-24'>
         <div className='conatainer mx-auto'>
-            <h2 className='section-title'>About me</h2>
+            <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>About me</h2>
+            <div>
+                {/** */}
+                <div>
+                    <DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark 
+                    w-[505px] h-[505px] bg-no-repeat relative' imgSrc='/about/developer.png'
+                    />
+                </div>
+            </div>
         </div>
     </section>
     );
