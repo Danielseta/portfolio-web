13a7779924dc7bb30c1651c9796085248c286ed0
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2024-08-03 00:45:53 -0700

    Bugs section 100%

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -7,11 +7,11 @@ const Contact = () => {
     <section>
       <div className='container mx-auto'>
         {/** text & illustration */}
-        <div className='grid xl:grid-cols-2 pt-12 xl:h-[488] mb-6 xl:mb-24'>
+        <div className='grid xl:grid-cols-2 '>
           {/** text} */}
           <div className='flex flex-col justify-center'>
             <div className='flex items-center gap-x-4 text-primary text-lg mb-4'>
-              <span className='w-[30px] h-[2px] bg-primary'></span>
+              <span className='w-[30px] h-[1px] bg-primary'></span>
               Say Hello🖐
             </div>
             <h1 className='h1 max-w-md mb-8'>Let's Work Together.</h1>
@@ -20,12 +20,12 @@ const Contact = () => {
             </p>
           </div>
           {/*  illustrations */}
-          <div className='hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat'></div>
+          <Form />
         </div>
         {/** info text & form */}
         <div className='grid xl:grid-cols2 mb-24 xl:mb-32'>
           {/* info text */}
-          <div className='flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg'>
+          <div className='flex flex-col gap-y-1 xl:gap-y-3 mb-12 xl:mb-24 text-base xl:text-lg'>
             {/** mail */}
             <div className='flex items-center gap-x-8'>
               <MailIcon size={18} className='text-primary' />
@@ -42,7 +42,7 @@ const Contact = () => {
               <div>+260 964 223 895</div>
             </div>
           </div>
-          <Form />
+          
         </div>
       </div>
     </section>
